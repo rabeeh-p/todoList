f0793b11647523a8d68a492e29e7ad4c35589b4c
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-11-08 11:39:08 +0530

    router installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  let [note,setNote]= useState([])
+  let [todo,setTodo]  = useState([])
+
+  let typingWord = (e)=>{
+    console.log('hello',e.target.value);
+    setNote(e.target.value)
+  }
+
+  let clickedIcon = ()=>{
+    if (note.trim() !== ''){
+
+      setTodo([...todo,note])
+    }
+  }
+  console.log(todo,'todo');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input onChange={typingWord}  type="text" placeholder="🖊️ Add item..." />
+        <i onClick={clickedIcon} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {Array.isArray(todo) && todo.map((item,index)=>{
+          return(
+            <div className="todo" key={index}>
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p> {item}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+
+          )
+        })}
+        
+      </div>
     </div>
   );
 }
